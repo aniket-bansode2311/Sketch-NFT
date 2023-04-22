cc96bd034d0a52331c80dc863d313b4bd39a062d
Aniket Bansode <aniket.bansode2311@gmail.com>
2023-04-22 13:46:16 +0530

    initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+async function main() {
+  const MyNFT = await ethers.getContractFactory("MyNFT");
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const myNFT = await MyNFT.deploy();
+  console.log("Contract deployed to address:", myNFT.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+//0xf482C43f79a97E9F1315981e1684c29d440a254E
